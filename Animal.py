de9dda2495089168227e1555c1b7e2e7438f891a
Ahmed <aahmedd9977@gmail.com>
2022-04-21 15:24:11 +0100

    Added more animals

--- Animal.py
+++ Animal.py
@@ -7,6 +7,8 @@ class Animal(ABC):
     color = None
     Age = None
     Speed = None
+    isDead = False
+    weight = None
 
     #Constructors
     def __init__(self):
@@ -38,8 +40,9 @@ class Animal(ABC):
     def grow(self):
         return "I am growing"
     
-    def dies(self):
-        return "I am dead RIP"
+    def dead(self):
+        self.isDead = True
+        return self.isDead
 
 class Mammal(Animal):
     #Attributes
@@ -69,9 +72,88 @@ class Mammal(Animal):
     def type(self):
         return self.value
 
+class Cat(Mammal):
+     #Attributes
+
+    name = None
+    collar_color = None
+
+    #Constructors
+    def __init__(self):
+        self.value = "Cat"
+    
+    #Methods
+    def type(self):
+        return self.value
+
+    def eat(self):
+        return "I eat mice"
+    
+    def move(self):
+        return "I walk or run"
+    
+    def breathe(self):
+        return "I breathe through my nose"
+    
+class Bat(Mammal):
+     #Attributes
+
+    name = None
+    isFlying = False
+
+    #Constructors
+    def __init__(self):
+        self.value = "Bat"
+    
+    #Methods
+    def type(self):
+        return self.value
+
+    def eat(self):
+        return "I eat meat"
+    
+    def move(self):
+        return "I fly"
+    
+    def breathe(self):
+        return "I breathe through my nose"
+    
+    def flying(self):
+        self.isFlying = True
+        return self.isFlying
+    
+    def landing(self):
+        self.isFlying = False
+        return self.isFlying
+    
+class Platypus(Mammal):
+     #Attributes
+
+    name = None
+
+    #Constructors
+    def __init__(self):
+        self.value = "Budgie"
+    
+    #Methods
+    def type(self):
+        return self.value
+
+    def eat(self):
+        return "I eat plants"
+    
+    def reproduce(self):
+        return "I lay eggs"
+    
+    def move(self):
+        return "I am amphibious"
+    
+    def breathe(self):
+        return "I breathe through my nose"
+
 class Bird(Animal):
     
-    # Attributes
+    #Attributes
     
     name = None
     wings = None
@@ -96,26 +178,3 @@ class Bird(Animal):
         
     def eat(self):
         return "I eat worms"
-
-class Cat(Mammal):
-     #Attributes
-
-    name = None
-    collar_color = None
-
-    #Constructors
-    def __init__(self):
-        self.value = "Cat"
-    
-    #Methods
-    def type(self):
-        return self.value
-
-    def eat(self):
-        return "I eat mice"
-    
-    def move(self):
-        return "I walk or run"
-    
-    def breathe(self):
-        return "I breathe through my nose"
--- AnimalRunner.py
+++ AnimalRunner.py
@@ -1,6 +1,13 @@
 from Animal import *
 
+# Animal: color, age, speed are global attributes
+# Mammal (Cat, Bat, Budgie): legs
+# Cat: collarcolor
+# Bird: name, wings, legs
+# 
+
 # Creating a cat named Bob
+
 Bob = Cat()
 Bob.name = "Bob"
 Bob.age = 5
@@ -14,16 +21,57 @@ print(Bob.move())
 print(Bob.breathe())
 print(Bob.reproduce())
 print(Bob.grow())
-print(Bob.dies())
+print(Bob.dead())
 print("\n")
 
-# Creating a bird
+# Creating a bat named Dracula
+
+Dracula = Bat()
+Dracula.name = "Dracula"
+Dracula.age = 5
+Dracula.legs = 4
+Dracula.wings = 2
+Dracula.speed = 20
+
+print(Dracula.name)
+print(Dracula.eat())
+print(Dracula.move())
+print(Dracula.breathe())
+print(Dracula.reproduce())
+print(Dracula.grow())
+print("I have", Dracula.wings, "wings")
+print("Dracula can fly:")
+print(Dracula.flying())
+print("Dracula's wings are injured, can he fly?")
+print(Dracula.landing())
+print(Dracula.dead())
+print("\n")
+
+# Creating a mammal Platypus named Platypus
+
+Platypus = Platypus()
+Platypus.name = "Platypus"
+Platypus.age = 5
+Platypus.legs = 4
+Platypus.speed = 20
+
+print(Platypus.name)
+print(Platypus.eat())
+print(Platypus.move())
+print(Platypus.breathe())
+print(Platypus.reproduce())
+print(Platypus.grow())
+print(Platypus.dead())
+print("\n")
+
+# Creating a bird named Birdie
 
 Birdie = Bird()
 Birdie.name = "Birdie"
 Birdie.age = 5
 Birdie.legs = 4
 Birdie.speed = 20
+Birdie.wings = 2
 Birdie.collar_color = "black"
 
 print(Birdie.name)
@@ -31,6 +79,7 @@ print(Birdie.eat())
 print(Birdie.move())
 print(Birdie.breathe())
 print(Birdie.reproduce())
+print("I have", Birdie.wings, "wings")
 print(Birdie.grow())
-print(Birdie.dies())
+print(Birdie.dead())
 
