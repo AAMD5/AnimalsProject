f55726d01c824fc8805a9637f8d6833b0ca6cae4
Ahmed <aahmedd9977@gmail.com>
2022-04-23 22:13:30 +0100

    modified solution slightly

--- ZooDisaster.py
+++ ZooDisaster.py
@@ -1,7 +1,11 @@
 from Animal import *
 
+# creating all objects
+
+
 grass = Grass()
 leaves = Leaves()
+#################### name, color, age, speed, isDead, weight
 antelope = Antelope("Antelope", "black", 5, 20, False, 20)
 bigfish = Bigfish("Big-fish", "black", 5, 20, False, 20)
 littlefish = Littlefish("Little-fish", "black", 5, 20, False, 20)
@@ -15,25 +19,10 @@ giraffe = Giraffe("Giraffe", "black", 5, 20, False, 20)
 lion = Lion("Lion", "black", 5, 20, False, 20)
 panda = Panda("Panda", "black", 5, 20, False, 20)
 
-# Input 
-
-# "fox,bug,chicken,grass,sheep" 
-
-# 1, fox can't eat bug, "fox,bug,chicken,grass,sheep" 
-# 2, bug can't eat anything, "fox,bug,chicken,grass,sheep" 
-# 3, chicken eats bug, "fox,chicken,grass,sheep" 
-# 4, fox eats chicken, "fox,grass,sheep" 
-# 5, fox can't eat grass, "fox,grass,sheep" 
-# 6, grass can't eat anything, "fox,grass,sheep" 
-# 7, sheep eats grass, "fox,sheep" 
-# 8, fox eats sheep, "fox" 
-# Output 
-# ["fox,bug,chicken,grass,sheep", "chicken eats bug", 
-#  "fox eats chicken", "sheep eats grass", "fox eats sheep", "fox"] 
-
-Zoo = [fox, bug, chicken, grass, sheep]
-
 def AnimalsToStrings(listOfAnimals):
+    
+    """ changes list of animal objects to list of animal names """
+    
     ZooStrings = []
     for animal in listOfAnimals:
         ZooStrings.append(animal.value)
@@ -41,43 +30,39 @@ def AnimalsToStrings(listOfAnimals):
     return ZooStrings
 
 def animalEat(zooList):
+    
+    """ function that dictates which animal eats which prey """
+    
     zooListLength = len(zooList)
-    eat = False
     for i in range(zooListLength): # from fox to sheep inclusive
         if zooList[i].eat(zooList[i-1])[0] == True and i != 0:
-            #print(zooList[i].value)
-            ateAnimal = zooList[i].eat(zooList[i-1])[1]
-            #print(ateAnimal)
-            zooList.remove(zooList[i-1])
-            #print(AnimalsToStrings(zooList))
-            break
-        
+            ateAnimal = zooList[i].eat(zooList[i-1])[1] 
+            zooList.remove(zooList[i-1]) # remove left eaten animal
+            break # to stop animal eating more edible animals to the left
+            
         i = 0 # reset loop
         if zooList[i].eat(zooList[i+1])[0] == True:
-            #print(zooList[i].value)
             ateAnimal = zooList[i].eat(zooList[i+1])[1]
-            #print(ateAnimal)
-            zooList.remove(zooList[i+1])
-            #print(AnimalsToStrings(zooList))
-            break
-    
+            zooList.remove(zooList[i+1]) # remove right eaten animal
+            break # to stop animal eating more edible animals to the right
+        
+            
     return ateAnimal
          
-def eatStory(zooList):
+def eatStory(preys):
+    
+    """ story of which animal ate which prey """
     
     lastAnimal = False
-    story = []
+    story = [AnimalsToStrings(preys)]
     while not lastAnimal:
-        a = animalEat(Zoo)
-        story.append(a)
-        if len(Zoo) == 1:
-            story.append(Zoo[0].value)
+        animalEatPrey = animalEat(preys)
+        story.append(animalEatPrey)
+        if len(preys) == 1: # only one animal left
+            story.append(AnimalsToStrings(preys))
             lastAnimal = True
 
     return story
-        
+     
+Zoo = [fox, bug, chicken, grass, sheep]   
 print(eatStory(Zoo))
-    
-# Output 
-# ["fox,bug,chicken,grass,sheep", "chicken eats bug", 
-#  "fox eats chicken", "sheep eats grass", "fox eats sheep", "fox"] 
